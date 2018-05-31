a27a50a8c2c2c2c0366a2dd169af5372dcaf2efb
ybotwe <ybotwe@gmail.com>
2018-05-31 11:30:42 +0000

    functionality for viewrooms and reports.

---
+++ HMS/static/reportContent.py
@@ -0,0 +1,8 @@
+reportContent = {
+    "totRooms": "Total number of rooms in ",
+    "totStu": "Total number of students in ",
+    "totStuPaid": "Total number of students who have fully paid in ",
+    "totFullRooms": "Total number of fully occupied rooms in ",
+    "totMaleStu": "Total number of males in ",
+    "totFemStu": "Total number of females in ",
+}
