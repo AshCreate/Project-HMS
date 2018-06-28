a60095d215eeb54f47436dd916cf7f4a69827dc6
ybotwe <ybotwe@gmail.com>
2018-06-28 11:47:21 +0000

    defensive programming for all admin and student pages

--- HMS/routes.py
+++ HMS/routes.py
@@ -1,6 +1,6 @@
 import os
 import secrets
-from PIL import Image
+from PIL import *
 from flask import render_template, url_for, flash, redirect, request, abort
 from HMS import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
